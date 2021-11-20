4c33275c5fe35c8aabca81b130d16a3de4ec5746
Nguyen Huu Phuoc <nguyenhuuphuocit97@gmail.com>
2021-11-20 17:56:16 +0700

    :ok_hand: chore: add system service on api and custom file env #8

--- src/main/java/com/phuocnguyen/app/ngxblobsuaa/NgxBlobsUaaApplication.java
+++ src/main/java/com/phuocnguyen/app/ngxblobsuaa/NgxBlobsUaaApplication.java
@@ -2,12 +2,21 @@ package com.phuocnguyen.app.ngxblobsuaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NgxBlobsUaaApplication {
 
+    private static ConfigurableApplicationContext context;
+
     public static void main(String[] args) {
-        SpringApplication.run(NgxBlobsUaaApplication.class, args);
+        // SpringApplication.run(NgxBlobsUaaApplication.class, args);
+        NgxBlobsUaaApplication.context = SpringApplication.run(NgxBlobsUaaApplication.class, args);
+    }
+
+    public static void restart() {
+        context.close();
+        NgxBlobsUaaApplication.context = SpringApplication.run(NgxBlobsUaaApplication.class);
     }
 
 }
--- src/main/java/com/phuocnguyen/app/ngxblobsuaa/controller/UsersSelfController.java
+++ src/main/java/com/phuocnguyen/app/ngxblobsuaa/controller/UsersSelfController.java
@@ -1,10 +1,12 @@
 package com.phuocnguyen.app.ngxblobsuaa.controller;
 
 import com.phuocnguyen.app.ngxblobssrv.controllers.NgxBaseController;
-import com.sivaos.Model.ObjectEnumeration.Original.Privileges;
+import com.phuocnguyen.app.ngxblobsuaa.NgxBlobsUaaApplication;
 import com.sivaos.Model.Response.SIVAResponseDTO;
 import com.sivaos.Utils.LoggerUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +23,10 @@ import static com.ngxsivaos.variable.RoutesApiVariable.ROUTES_API_USER_SELF;
 @RequestMapping(value = ROUTES_API_USER_SELF)
 public class UsersSelfController extends NgxBaseController {
 
+    private ApplicationContext applicationContext;
+
     @Value("${spring.profiles.active}")
     private String profileActive;
-
 
     @PostConstruct
     public void init() {
@@ -32,13 +35,35 @@ public class UsersSelfController extends NgxBaseController {
         this.setProfile(profileActive);
     }
 
-    @GetMapping(value = "/info")
+    @SuppressWarnings("SameParameterValue")
+    private void initiateAppShutdown(int returnCode) {
+        SpringApplication.exit(applicationContext, () -> returnCode);
+    }
+
+    @GetMapping("/info")
     public @ResponseBody
     ResponseEntity<?> findSelfInfo() {
-        logger.info("Json auth request: {}", LoggerUtils.toJson(authSecuritiesContextRequest));
-        logger.info("Users request: {}", LoggerUtils.toJson(userRequest));
-        logger.info("User has permission: {}", LoggerUtils.toJson(this.userRequest.getPrivileges()));
-        logger.info("User has read: {}", this.ngxCommonService.isUserHasPermission(this.userRequest, Privileges.IS_APPEND_TO));
+        logger.info("Users request: {}, has permission: {}",
+                LoggerUtils.toJson(userRequest),
+                LoggerUtils.toJson(this.userRequest.getPrivileges()));
         return new ResponseEntity<>(SIVAResponseDTO.buildSIVAResponse(userRequest), HttpStatus.OK);
+    }
+
+    @GetMapping("/restart")
+    void restart2() {
+        Thread restartThread = new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+                NgxBlobsUaaApplication.restart();
+            } catch (InterruptedException ignored) {
+            }
+        });
+        restartThread.setDaemon(false);
+        restartThread.start();
+    }
+
+    @RequestMapping("/shutdown")
+    public void shutdown() {
+        initiateAppShutdown(0);
     }
 }
